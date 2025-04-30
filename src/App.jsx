553429598de7d605c3c7b49f38193e5ea447a3ba
Jose <josefbautista94@gmail.com>
2025-04-29 22:02:50 -0400

    Completed the requirements for the lab might make it more dynamic later

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-// import EmployeePage from "./pages/EmployeePage";
-
+import EmployeePage from "./pages/EmployeePage";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        {/* <Route path="/employee/:id" element={<EmployeePage />} /> */}
-      </Routes>
-    </Router>
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        maxWidth: "1200px",
+        margin: "0 auto",
+        color: "white",
+        boxSizing: "border-box",
+        borderLeft: "1px solid gray" 
+      }}
+    >
+      <div
+        style={{
+          width: "40%",
+          borderRight: "1px solid gray",
+          boxSizing: "border-box",
+          overflowY: "auto", // scrolls if list gets long
+        }}
+      >
+        <HomePage />
+      </div>
+      <div
+        style={{
+          width: "60%",
+          boxSizing: "border-box",
+          overflowY: "auto",
+          borderRight:"1px solid gray"
+        }}
+      >
+        <EmployeePage />
+      </div>
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,20 @@ const Header = ({title}) => { // creating function called header
     return( // we're going to return jsx
 
         //The first {} means "this is JavaScript.", The second {} is the object with the style info.
-        <header style = {{padding: "20px", background: "#333", color :"white"}}> 
-            <h1>{title}</h1> {/* This is saying, "show whatever the title is inside an h1 tag." If title is “Employee Directory,” it’ll show up as big text: Employee Directory.*/}
+        <header      style={{
+            padding: "20px",
+            background: "#1f1f1f",
+            color: "white",
+            width: "100%",
+            height: "80px", // Fix both header heights
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center", // center the text inside
+            boxSizing: "border-box" // make sure padding doesn't push it beyond width
+          }}
+    > 
+                <h1 style={{ fontSize: "24px", margin: 0 }}>{title}</h1>
+                {/* This is saying, "show whatever the title is inside an h1 tag." If title is “Employee Directory,” it’ll show up as big text: Employee Directory.*/}
         </header>
     )
 }
